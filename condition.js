180b0aa8a988a9042321b31ac3cfa7d2ae8ff17c
valdev5 <151469215+valdev5@users.noreply.github.com>
2023-11-19 21:38:28 +0100

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,21 @@
+
+let balance = 2000; // Solde du compte
+let amount = prompt("Saisir le montant du virement"); // Montant du virement (à saisir par l'utilisateur)
+let authorization = ""; // Autorisation
+
+
+ amount;
+ if (balance > amount) {
+    balance = balance - amount;
+    authorization = "autorisé";    
+}
+if (balance < amount) {
+    balance = balance - amount;
+    authorization = "non autorisé";
+    
+}
+
+
+
+
+console.submit(`Vous avez choisi de virer ${Number(amount).toFixed(2)} € de votre compte. Votre nouveau solde sera de ${Number(balance).toFixed(2)} € après le virement qui est ${authorization}.`);
