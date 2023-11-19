4e7445ed5d87655f603dfd527ca4166d7307946a
valdev5 <151469215+valdev5@users.noreply.github.com>
2023-11-19 21:35:08 +0100

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,8 @@
+let lastname = "Potter";
+let firstname = "Harry";
+let age = 43 ;
+let house = "Gryffindor";
+let points = "2500";
+
+
+console.submit(`${firstname} ${lastname}, ${age} ans, ${house}, ${points} points`);
